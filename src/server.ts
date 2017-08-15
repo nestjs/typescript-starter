e7bb2fb8ed61e44b3edf47538e5c7168ed17f34d
odykyi <24225497+odykyi@users.noreply.github.com>
2017-08-15 17:58:31 +0300

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -2,4 +2,4 @@ import { NestFactory } from '@nestjs/core';
 import { ApplicationModule } from './modules/app.module';
 
 const app = NestFactory.create(ApplicationModule);
-app.listen(3000, () => console.log('Application is listening on port 3000.'));
+app.then(instance => instance.listen(3000, () => console.log('Application is listening on port 3000')));
