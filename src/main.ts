fbf61e5d76ee97ecb6724626233e6676dbed4484
Kamil Myśliwiec <mail@kamilmysliwiec.com>
2018-01-02 16:14:44 +0100

    update(@nestjs) fit style guide

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import { NestFactory } from '@nestjs/core';
-import { ApplicationModule } from './modules/app.module';
+import { ApplicationModule } from './app.module';
 
 async function bootstrap() {
 	const app = await NestFactory.create(ApplicationModule);
