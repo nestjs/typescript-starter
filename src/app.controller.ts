f346875387bb05b2344a93697b5cab3c029e37d6
Kamil Myśliwiec <mail@kamilmysliwiec.com>
2018-03-26 21:19:56 +0200

    feature: update to nest v5

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,12 @@
 import { Get, Controller } from '@nestjs/common';
+import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
+  constructor(private readonly appService: AppService) {}
+
   @Get()
   root(): string {
-    return 'Hello World!';
+    return this.appService.root();
   }
 }
