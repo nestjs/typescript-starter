cde3a68c8c03cba9a0b0515731b86557517362d6
Ben Woodward <ben@webantic.co.uk>
2018-11-23 16:14:32 +0000

    Add ClassSerializationInterceptor and ValidationPipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,40 @@
-import { Module } from '@nestjs/common';
+import {
+  ClassSerializerInterceptor,
+  Module,
+  ValidationPipe,
+  OnModuleInit,
+  Logger,
+} from '@nestjs/common';
+import { APP_INTERCEPTOR, APP_PIPE, Reflector } from '@nestjs/core';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from './config/config.module';
+import { ConfigService } from './config/config.service';
 
 @Module({
   imports: [ConfigModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    {
+      provide: APP_INTERCEPTOR,
+      useFactory: (reflector: Reflector) => {
+        new ClassSerializerInterceptor(reflector);
+      },
+      inject: [Reflector],
+    },
+    {
+      provide: APP_PIPE,
+      useFactory: (config: ConfigService) => {
+        new ValidationPipe({
+          disableErrorMessages: config.isProdEnvironment,
+          transform: true,
+          whitelist: true,
+        });
+      },
+      inject: [ConfigService],
+    },
+    AppService,
+  ],
 })
 export class AppModule {}
