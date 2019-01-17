76be846ba9edb3223a65aad70651860aae3d142a
Robert Sobolczyk <sobolczyk.robert@gmail.com>
2019-01-17 15:55:39 +0100

    Unexpected token during build task after adding TypeOrm entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
+import {TypeOrmModule} from '@nestjs/typeorm';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import {Photo} from './photo.entity';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot(),
+    TypeOrmModule.forFeature([Photo]),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
