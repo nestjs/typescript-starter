f586015774d2cb86d338d98019ee473d9334c86d
Kamil Myśliwiec <mail@kamilmysliwiec.com>
2020-08-23 15:05:58 +0200

    lint(): disable explicit module boundary types

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,6 +19,7 @@ module.exports = {
   rules: {
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
   },
 };
