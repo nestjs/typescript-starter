1f5a8180a5630f1aa2e9e01a0c8d68d83b6f9d1c
Kamil Myśliwiec <mail@kamilmysliwiec.com>
2020-11-04 11:00:44 +0100

    chore(): update deps, eslint configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,10 +6,9 @@ module.exports = {
   },
   plugins: ['@typescript-eslint/eslint-plugin'],
   extends: [
-    'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
-    'prettier',
     'prettier/@typescript-eslint',
+    'plugin:prettier/recommended',
   ],
   root: true,
   env: {
