be10e65a893ebb6cf29bccaa2bafebccfbcbf08f
Wanchai Nupinit <wanchai.23.chai@gmail.com>
2021-01-14 17:51:15 +0700

    add typeorm

--- config/configuration.ts
+++ config/configuration.ts
@@ -1,4 +1,5 @@
 export default () => ({
+  env: process.env.NODE_ENV || 'development',
   port: parseInt(process.env.PORT, 10) || 3000,
   database: {
     host: process.env.DATABASE_HOST || process.env.DB_URL,
@@ -6,6 +7,7 @@ export default () => ({
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     type: process.env.DB_TYPE || 'postgres',
-    uri: process.env.DB_URI
+    uri: process.env.DB_URI,
+    database_name: process.env.DB_NAME
   }
 });
