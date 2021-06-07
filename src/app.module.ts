42f2027d47da61c33d17574b8d33e7f1d9f0273f
Bruno Leal <calhau.bruno88@gmail.com>
2021-06-07 13:35:38 -0300

    feat: add joi to our application

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,21 @@
+import * as Joi from '@hapi/joi';
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({
+    validationSchema: Joi.object({
+      POSTGRES_HOST: Joi.string().required(),
+      POSTGRES_PORT: Joi.number().required(),
+      POSTGRES_USER: Joi.string().required(),
+      POSTGRES_PASSWORD: Joi.string().required(),
+      POSTGRES_DB: Joi.string().required(),
+      PORT: Joi.number(),
+    })
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
