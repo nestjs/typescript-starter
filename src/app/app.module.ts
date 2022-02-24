c63bf84e99a46a249bb60452c1252c390374ed4e
Jardel <jardel1101@gmail.com>
2022-02-24 15:29:59 -0300

    app na raíz do src

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,20 +2,26 @@ import { join } from 'path'
 
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo'
 import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
 import { GraphQLModule } from '@nestjs/graphql'
 
-//import { AppController } from './app.controller'
+import { DatabaseModule } from 'src/database/database.module'
+import { ModulesModule } from 'src/modules/modules.module'
+
 import { AppService } from './app.service'
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
+    DatabaseModule,
+    ModulesModule,
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
       sortSchema: true,
     }),
   ],
-  //controllers: [AppController],
+
   providers: [AppService],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { NestFactory } from '@nestjs/core'
 
-import { AppModule } from './modules/app/app.module'
+import { AppModule } from './app/app.module'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
