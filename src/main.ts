f4260f941a08637c7a976d0e9661cd0ca670a99c
Jardel <jardel1101@gmail.com>
2022-02-24 18:31:15 -0300

    Server PORT

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,17 @@
+import { Logger } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
 
 import { AppModule } from './base/app/app.module'
 
+const PORT = Number(process.env.PORT || 3001)
+
 async function bootstrap() {
+  const logger = new Logger('Bootstrap')
+
   const app = await NestFactory.create(AppModule)
-  await app.listen(3000)
+  await app.listen(PORT).then(() => {
+    logger.log(`🚀 Server started on port ${PORT}`)
+  })
 }
 
 bootstrap()
