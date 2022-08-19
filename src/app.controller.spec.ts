5bacc0871cd31f7f4aca11bd2a8e348c78f6071f
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2022-08-19 12:57:54 -0500

    chore: remove unnecessary type

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -14,7 +14,7 @@ describe('AppController', () => {
 
   describe('getHello', () => {
     it('should return "Hello World!"', () => {
-      const appController = app.get<AppController>(AppController);
+      const appController = app.get(AppController);
       expect(appController.getHello()).toBe('Hello World!');
     });
   });
