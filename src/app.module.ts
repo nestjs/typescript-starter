9ca2a39907fd39f8de397e257583fc3715ee38c8
Abdul Rehman <79081209+abdulrehman909@users.noreply.github.com>
2022-09-21 06:52:46 +0500

    initial commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [AuthModule],
 })
 export class AppModule {}
