2454b427dfa55b517924108522be6019080e4bba
Isis de Oliveira <isisd.oliveira@hotmail.com>
2022-10-10 23:44:25 -0300

    Cobertura de testes método Create e FindAll

--- src/item/item.controller.spec.ts
+++ src/item/item.controller.spec.ts
@@ -3,18 +3,45 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { Item } from './entities/item.entity';
 import { ItemController } from './item.controller';
 import { ItemService } from './item.service';
+import { CreateItemDto } from './dto/create-item.dto';
 
 describe('ItemController', () => {
   let controller: ItemController;
   let service: ItemService;
 
-  const itemEntityList: Item[]= [
-    new Item({id: 1, name:"carbox", description:"carboxiterapia", quantity:3}),
-    new Item({id: 2, name:"drenagem", description:"drenagem linfática", quantity:1}),
-    new Item({id: 3, name:"depilação", description:"depilação a laser", quantity:10}),
+  const itemEntityList: Item[] = [
+    new Item({
+      id: 1,
+      name: 'carbox',
+      description: 'carboxiterapia',
+      quantity: 3,
+    }),
+    new Item({
+      id: 2,
+      name: 'drenagem',
+      description: 'drenagem linfática',
+      quantity: 1,
+    }),
+    new Item({
+      id: 3,
+      name: 'depilação',
+      description: 'depilação a laser',
+      quantity: 10,
+    }),
   ];
-  
-  const newItem = new Item({id: 1, name:"carbox", description:"carboxiterapia", quantity:3});
+
+  const newItem = new Item({
+    id: 1,
+    name: 'carbox',
+    description: 'carboxiterapia',
+    quantity: 3,
+  });
+
+  const body = new CreateItemDto();
+
+  body.description = 'Carboxterapia';
+  body.name = 'Carbox';
+  body.quantity = 5;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -22,8 +49,8 @@ describe('ItemController', () => {
       providers: [
         {
           provide: ItemService,
-          useValue:{
-            create: jest.fn(),
+          useValue: {
+            create: jest.fn().mockRejectedValue(itemEntityList[0]),
             findAll: jest.fn().mockResolvedValue(itemEntityList),
             findById: jest.fn().mockResolvedValue(newItem),
             update: jest.fn(),
@@ -33,14 +60,53 @@ describe('ItemController', () => {
     }).compile();
 
     controller = module.get<ItemController>(ItemController);
-    service = module.get<ItemService>(ItemService)
+    service = module.get<ItemService>(ItemService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
     expect(service).toBeDefined();
   });
-  
+
+  describe('create', () => {
+    it('should create a item successfully', async () => {
+      //Act
+      const result = await controller.create(body);
+
+      //Assert
+      console.log(result);
+      console.log(body);
+      expect(result).toEqual(newItem);
+      expect(service.create).toHaveBeenCalledTimes(1);
+    });
+
+    it('should throw an exception', () => {
+      //Arrange
+      jest.spyOn(controller, 'create').mockRejectedValueOnce(new Error());
+
+      //Assert
+      expect(controller.create).rejects.toThrowError();
+    });
+  });
+
+  describe('findAll', () => {
+    it('should return a array list successfully', async () => {
+      //Act
+      const result = await controller.findAll();
+      //Assert
+      expect(result).toEqual(itemEntityList);
+      expect(service.findAll).toHaveBeenCalledTimes(1);
+    });
+
+    it('should throw an exception', () => {
+      //Arrange
+      jest.spyOn(controller, 'findAll').mockRejectedValueOnce(new Error());
+
+      //Assert
+      expect(controller.findAll()).rejects.toThrowError();
+    });
+  });
+
   describe('findById', () => {
     it('should return a item by id successfully', async () => {
       //Act
@@ -48,30 +114,27 @@ describe('ItemController', () => {
       //Assert
       expect(result).toEqual(itemEntityList[0]);
       expect(service.findById).toHaveBeenCalledTimes(1);
-    })
+    });
 
     it('should throw an exception', () => {
-      //Arrange 
+      //Arrange
       jest.spyOn(controller, 'findById').mockRejectedValueOnce(new Error());
-      
+
       //Assert
       expect(controller.findById(1)).rejects.toThrowError();
-    })
-  })
-
-  
+    });
+  });
 
   describe('update', () => {
     it('should return a item by id successfully', async () => {
-      //Arrange 
+      //Arrange
       //Act
       //Assert
-    })
+    });
     it('should throw an exception', async () => {
-      //Arrange 
+      //Arrange
       //Act
       //Assert
-    })
-  })
-
+    });
+  });
 });
