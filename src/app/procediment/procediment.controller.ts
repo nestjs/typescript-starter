21c2c823db7988b7a5b3e599742266b742d655b9
Nize <edeniziacost@gmail.com>
2022-10-23 14:15:53 -0300

    Add format in class

--- src/app/procediment/procediment.controller.ts
+++ src/app/procediment/procediment.controller.ts
@@ -10,7 +10,7 @@ import {
   HttpCode,
   ParseIntPipe,
   Request,
-  UseGuards
+  UseGuards,
 } from '@nestjs/common';
 import { ProcedimentService } from './procediment.service';
 import { CreateProcedimentDto } from './dto/create-procediment.dto';
@@ -19,7 +19,7 @@ import { JwtAuthGuard } from 'src/app/auth/jwt-auth.guard';
 
 @Controller('procediment')
 export class ProcedimentController {
-  constructor(private readonly procedimentService: ProcedimentService) { }
+  constructor(private readonly procedimentService: ProcedimentService) {}
 
   @Get('/getall')
   async index() {
@@ -44,7 +44,7 @@ export class ProcedimentController {
 
   @Delete('delete/:id')
   @HttpCode(HttpStatus.NO_CONTENT)
-  async destroy(@Param('id', new ParseIntPipe) id: number){
+  async destroy(@Param('id', new ParseIntPipe()) id: number) {
     return await this.procedimentService.destroy(id);
   }
 }
