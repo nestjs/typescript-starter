ddf87dd46c8ea610f782d32cd28a467dcb4d7f94
Mohan Krishna Karki <12503500+mohankrishnakarki@users.noreply.github.com>
2022-11-11 14:03:54 +0545

    initial commit

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,26 @@
 import { Injectable } from '@nestjs/common';
-
+const momentTZ = require('moment-timezone');
+const moment = require('moment');
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getHello(): any {
+    const timezone = 'America/Chicago';
+    const date = moment().utc().format('YYYY-MM-DD HH:m:s');
+    const estDate = momentTZ(date).tz(timezone).format('YYYY-MM-DD HH:m:s');
+    if (date === estDate) {
+      return {
+        message: 'Not converting',
+        estDate: estDate,
+        estTiezone: timezone,
+        UTCDate: date,
+      };
+    } else {
+      return {
+        message: 'Converting',
+        estDate: estDate,
+        estTiezone: timezone,
+        UTCDate: date,
+      };
+    }
   }
 }
