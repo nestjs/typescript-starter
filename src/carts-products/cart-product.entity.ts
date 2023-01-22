8606c8109d7562ebf062a4f31cf7e1b3e14fb958
MarekKluk <105215275+MarekKluk@users.noreply.github.com>
2023-01-22 21:19:24 +0100

    Setting products and carts as primary columns

--- src/carts-products/cart-product.entity.ts
+++ src/carts-products/cart-product.entity.ts
@@ -1,17 +1,23 @@
-import {Column, Entity, ManyToOne, PrimaryColumn, PrimaryGeneratedColumn} from 'typeorm';
+import {Column, Entity, JoinColumn, ManyToOne, PrimaryColumn} from 'typeorm';
 import Product from '../products/product.entity';
 import Cart from '../carts/cart.entity';
 
 @Entity()
 class CartProduct {
 
-  @PrimaryColumn()
   @ManyToOne(() => Product)
+  @JoinColumn({ name: 'productId' })
   public product: Product;
 
   @PrimaryColumn()
+  public productId: number;
+
+  @JoinColumn({ name: 'cartId' })
   @ManyToOne(() => Cart, (cart) => cart.cartProduct)
   public cart: Cart;
+
+  @PrimaryColumn()
+  public cartId: number;
 
   @Column()
   public numberOfProducts: number;
