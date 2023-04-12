363687b38279b1751bb2905fdb4c8a9d42118253
Wendy-wyt <wuyuting0101@gmail.com>
2023-04-12 10:52:06 -0400

    connection fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+
+import { TasksModule } from './tasks/tasks.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TasksModule,
+    MongooseModule.forRoot('mongodb://ralabs:ralabs12345@ac-itetvwv-shard-00-02.ggwtpnj.mongodb.net:27017,ac-itetvwv-shard-00-01.ggwtpnj.mongodb.net:27017,ac-itetvwv-shard-00-00.ggwtpnj.mongodb.net:27017/RALabs?w=majority&retryWrites=true&tls=true&authMechanism=SCRAM-SHA-1')
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
