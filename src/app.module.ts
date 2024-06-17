cf8c001af060069beac275224db715f41e630549
Cabreira97 <thiagovieiramachado97@gmail.com>
2024-06-17 00:23:14 -0300

    ✨ Initial Commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { EventModule } from './event/event.module';
+import { OrganizerModule } from './organizer/organizer.module';
+import { VolunteerModule } from './volunteer/volunteer.module';
 
 @Module({
-  imports: [],
+  imports: [EventModule, OrganizerModule, VolunteerModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('API-MAIN  --- ConnectTech')
+    .setDescription('Connect Tech ')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
