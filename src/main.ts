50ef7ce5c007ed0a48c4862e355d572ca1567a4d
Thiago Vieira <53668902+Cabreira97@users.noreply.github.com>
2024-12-12 00:28:21 +0000

    add url api

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
 
   // Configurar CORS usando enableCors
   app.enableCors({
-    origin: 'http://localhost:3001', // Adicione a origem permitida aqui
+    origin: 'http://localhost:3001' || 'https://connect-tech-api-main.vercel.app', // Adicione a origem permitida aqui
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true, // Se precisar permitir cookies entre diferentes origens
   });
